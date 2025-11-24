d9c90f50ceaa38b9279c2d8994ee740aa4b0854a
HarryEddward <he0780070@gmail.com>
2025-11-24 11:42:30 +0100

    Push all project ro production testing

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,23 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.cantech.pro",
+      },
+      {
+        protocol: "https",
+        hostname: "cantech.pro",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["https://www.cantech.pro", "https://cantech.pro"],
+    },
+  },
 };
  
 const withNextIntl = createNextIntlPlugin();
